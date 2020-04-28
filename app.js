0745b40f4fcf72af9a68efa29e761c9ac0202672
Louise Norris <louisenorris73@yahoo.co.uk>
2020-04-28 22:30:09 +0100

    code notes not related to game, practice purposes

--- app.js
+++ app.js
@@ -68,3 +68,36 @@ startGameBtn.addEventListener('click', () => {
     alert(message);
     gameIsRunning = false;
 });
+
+//not related to the game
+
+// const combine = (resultHandler, operation, ...numbers) => {
+//     const validateNumber = (number) => {
+//         return isNaN(number) ? 0 : number;
+//     }
+//     let sum = 0;
+//     for (const num of numbers) {
+//         if (operation === 'ADD') {
+//             sum += validateNumber(num);
+//         } else {
+//             sum -= validateNumber(num);
+//         }
+//     }
+//     resultHandler(sum);
+// }
+
+// const subtractUp = function(resultHandler, ...numbers) {
+//     let sum = 0;
+//     for (const num of numbers) {
+//         sum -= num;
+//     }
+//     resultHandler(sum);
+// };
+
+// const showResult = (messageText, result) => {
+//     alert(messageText + ' ' + result);
+// }
+
+// combine(showResult.bind(this, 'The result after adding all numbers is:'), 'ADD', 5, 10, -3, 6, 10);
+// combine(showResult.bind(this, 'The result after adding all numbers is:'), 'ADD', 5, 10, -3, 6, 10, 25, 88);
+// combine(showResult.bind(this, 'The result after subtracting all numbers is:'), 'SUBTRACT', 1, 10, 15, 20);
