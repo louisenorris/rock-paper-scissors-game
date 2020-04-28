91443ea3b1458a4f04f63212fece9cc53a922c1d
Louise Norris <louisenorris73@yahoo.co.uk>
2020-04-28 09:19:37 +0100

    added get winner functionality to complete core game logic

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -31,12 +34,28 @@ const getComputerChoice = function() {
     }
 };
 
+const getWinner = function(cChoice, pChoice) {
+    if (cChoice === pChoice) {
+        return RESULT_DRAW;
+    } else if (
+        cChoice === ROCK && pChoice === PAPER ||
+        cChoice === PAPER && pChoice === SCISSORS ||
+        cChoice === SCISSORS && pChoice === ROCK
+        ) {
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+};
+
 startGameBtn.addEventListener('click', function() {
     if (gameIsRunning) {
         return;
     }
     gameIsRunning = true;
     console.log('Game is starting...');
-    const playerSelection = getPlayerChoice();
+    const playerChoice = getPlayerChoice();
     const computerChoice = getComputerChoice();
+    const winner = getWinner(computerChoice, playerChoice);
+    console.log(winner)
 });
