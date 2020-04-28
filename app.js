44bac087962ecab98056f3f3f8264b0e0f98c782
Louise Norris <louisenorris73@yahoo.co.uk>
2020-04-28 09:10:36 +0100

    added computer choice functionlity

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ const getPlayerChoice = function() {
     return selection;
 };
 
+const getComputerChoice = function() {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+};
+
 startGameBtn.addEventListener('click', function() {
     if (gameIsRunning) {
         return;
@@ -27,5 +38,5 @@ startGameBtn.addEventListener('click', function() {
     gameIsRunning = true;
     console.log('Game is starting...');
     const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const computerChoice = getComputerChoice();
 });
